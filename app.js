f0010f9a48877ecbf9225697dcba0aa06e4e7e92
shikamusenge <shikamusenge720@gmail.com>
2024-05-10 09:04:07 +0200

    second commit full app

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const usersRoute = require("./router/users");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const PORT = 3005;
